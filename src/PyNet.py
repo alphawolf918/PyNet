af1b398994ce6ef7d9293a97a29c5dfb256e4439
Zollern Wolf <admin@zollernverse.org>
2018-11-12 13:07:26 -0500

    Logs network speeds to DB now
    .

--- src/PyNet.py
+++ src/PyNet.py
@@ -1,4 +1,5 @@
 import speedtest
+import pyodbc
 
 speedtester = speedtest.Speedtest()
 
@@ -6,11 +7,28 @@ speedtester.get_best_server()
 
 megabits = 1048576
 
-downloadSpeed = int(speedtester.download()) / megabits
-uploadSpeed = int(speedtester.upload()) / megabits
+downloadSpeed = speedtester.download() / megabits
+uploadSpeed = speedtester.upload() / megabits
 
 downloadSpeed = round(downloadSpeed, 2)
 uploadSpeed = round(uploadSpeed, 2)
+
+downloadSpeed = str(downloadSpeed)
+uploadSpeed = str(uploadSpeed)
+
+sqlServer = "localhost\SQLEXPRESS01"
+sqlTable = "net_speeds"
+sqlDb = "PyNet"
+
+sqlCon = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                      "Server=" + sqlServer + ";"
+                      "Database=" + sqlDb + ";"
+                      "Trusted_Connection=yes;")
+
+cursor = sqlCon.cursor()
+cursor.execute("INSERT INTO " + sqlTable + " (downloadSpeed, uploadSpeed) VALUES (" + downloadSpeed + ", " + uploadSpeed + ")")
+cursor.commit()
+cursor.close()
 
 print("-------")
 print("Download Speed: " + str(downloadSpeed) + " mb/s")
