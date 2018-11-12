476d099f1d522b4baad3a99af95c589fdec9dadc
Zollern Wolf <admin@zollernverse.org>
2018-11-12 13:16:23 -0500

    Prints out what it's doing now.
    .

--- src/PyNet.py
+++ src/PyNet.py
@@ -1,13 +1,18 @@
 import speedtest
 import pyodbc
 
+print("Connecting to SpeedTest...")
 speedtester = speedtest.Speedtest()
 
+print("Retrieving best possible server...")
 speedtester.get_best_server()
 
 megabits = 1048576
 
+print("Calculating download speed...")
 downloadSpeed = speedtester.download() / megabits
+
+print("Calculating upload speed...")
 uploadSpeed = speedtester.upload() / megabits
 
 downloadSpeed = round(downloadSpeed, 2)
@@ -20,17 +25,21 @@ sqlServer = "localhost\SQLEXPRESS01"
 sqlTable = "net_speeds"
 sqlDb = "PyNet"
 
+print("Connecting to database...")
+
 sqlCon = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
                       "Server=" + sqlServer + ";"
                       "Database=" + sqlDb + ";"
                       "Trusted_Connection=yes;")
-
+print("Connected! Inserting network speed values...")
 cursor = sqlCon.cursor()
 cursor.execute("INSERT INTO " + sqlTable + " (downloadSpeed, uploadSpeed) VALUES (" + downloadSpeed + ", " + uploadSpeed + ")")
 cursor.commit()
+print("Values inserted, closing connection.")
 cursor.close()
+print("Connection closed.\n")
 
 print("-------")
 print("Download Speed: " + str(downloadSpeed) + " mb/s")
 print("Upload Speed: " + str(uploadSpeed) + " mb/s")
-print("-------")
+print("-------\n")
