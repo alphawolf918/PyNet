e09855895f3849b8bee640908aa5129a795b60d7
Zollern Wolf <admin@zollernverse.org>
2018-11-12 14:39:02 -0500

    Refactored and consolidated a few things.
    .

--- src/PyNet.py
+++ src/PyNet.py
@@ -4,6 +4,9 @@ import pyodbc
 print("Connecting to SpeedTest...")
 speedtester = speedtest.Speedtest()
 
+print("Getting set of closest servers...")
+speedtester.get_closest_servers()
+
 print("Retrieving best possible server...")
 speedtester.get_best_server()
 
@@ -15,31 +18,29 @@ downloadSpeed = speedtester.download() / megabits
 print("Calculating upload speed...")
 uploadSpeed = speedtester.upload() / megabits
 
-downloadSpeed = round(downloadSpeed, 2)
-uploadSpeed = round(uploadSpeed, 2)
-
-downloadSpeed = str(downloadSpeed)
-uploadSpeed = str(uploadSpeed)
+downloadSpeed = str(round(downloadSpeed, 2))
+uploadSpeed = str(round(uploadSpeed, 2))
 
 sqlServer = "localhost\SQLEXPRESS01"
 sqlTable = "net_speeds"
 sqlDb = "PyNet"
 
 print("Connecting to database...")
-
 sqlCon = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                      "Server=" + sqlServer + ";"
-                      "Database=" + sqlDb + ";"
-                      "Trusted_Connection=yes;")
+                        "Server=" + sqlServer + ";"
+                        "Database=" + sqlDb + ";"
+                        "Trusted_Connection=yes;")
+
 print("Connected! Inserting network speed values...")
 cursor = sqlCon.cursor()
 cursor.execute("INSERT INTO " + sqlTable + " (downloadSpeed, uploadSpeed) VALUES (" + downloadSpeed + ", " + uploadSpeed + ")")
 cursor.commit()
 print("Values inserted, closing connection.")
 cursor.close()
+sqlCon.close()
 print("Connection closed.\n")
 
-print("-------")
-print("Download Speed: " + str(downloadSpeed) + " mb/s")
-print("Upload Speed: " + str(uploadSpeed) + " mb/s")
-print("-------\n")
+print("--------------")
+print("Download Speed: " + downloadSpeed + " Mbps")
+print("Upload Speed: " + uploadSpeed + " Mbps")
+print("--------------\n")
