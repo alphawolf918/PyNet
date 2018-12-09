09ff17d7e97ba4503f0a29fe5224550035fda6b8
Zollern Wolf <admin@zollernverse.org>
2018-12-09 10:17:44 -0500

    Starts SQL service automatically
    Also added try except

--- src/PyNet.py
+++ src/PyNet.py
@@ -1,9 +1,21 @@
 import speedtest
 import pyodbc
+import ctypes
+import win32serviceutil as winServ
+
+ctypes.windll.kernel32.SetConsoleTitleW("PyNet")
+
+sqlServiceName = "MSSQL$SQLEXPRESS01"
+sqlServiceStatus = winServ.QueryServiceStatus(sqlServiceName)[1]
+
+if sqlServiceStatus != 4:
+    print("Starting SQL Server Service...")
+    winServ.StartService(sqlServiceName)
+    print("SQL Server Service started successfully.\n")
 
 print("Connecting to SpeedTest...")
 speedtester = speedtest.Speedtest()
-
+    
 print("Getting set of closest servers...")
 speedtester.get_closest_servers()
 
@@ -22,19 +34,22 @@ downloadSpeed = str(round(downloadSpeed, 2))
 uploadSpeed = str(round(uploadSpeed, 2))
 
 print("Connecting to database...")
-sqlCon = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                        "Server=localhost\SQLEXPRESS01;"
-                        "Database=PyNet;"
-                        "Trusted_Connection=yes;")
+try:
+    sqlCon = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                            "Server=localhost\SQLEXPRESS01;"
+                            "Database=PyNet;"
+                            "Trusted_Connection=yes;")
 
-print("Connected! Inserting network speed values...")
-cursor = sqlCon.cursor()
-cursor.execute("INSERT INTO net_speeds (downloadSpeed, uploadSpeed) VALUES (" + downloadSpeed + ", " + uploadSpeed + ")")
-cursor.commit()
-print("Values inserted, closing connection.")
-cursor.close()
-sqlCon.close()
-print("Connection closed.\n")
+    print("Connected! Inserting network speed values...")
+    cursor = sqlCon.cursor()
+    cursor.execute("INSERT INTO net_speeds (downloadSpeed, uploadSpeed) VALUES (" + downloadSpeed + ", " + uploadSpeed + ")")
+    cursor.commit()
+    print("Values inserted, closing connection.")
+    cursor.close()
+    sqlCon.close()
+    print("Connection closed.\n")
+except:
+    print("There was an error when connecting to the database.")
 
 print("--------------")
 print("Download Speed: " + downloadSpeed + " Mbps")
